3d354348bdb16a9cc6dfba73dd0d569694498207
leaaaaass <132161655+leaaaaass@users.noreply.github.com>
2024-12-30 17:29:23 +0100

    buttons

--- Assets/Scripts/Platform system/Platform.cs
+++ Assets/Scripts/Platform system/Platform.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System;
 
 public class Platform : MonoBehaviour
 {
     public List<Transform> floors;
     public int currentFloor;
     public int nextFloor;
+    public float floorHeight = 3f;
 
     public void SetNextFloor(int value)
     {
         nextFloor = value;
+    }
+
+    internal Vector3 GetFloorPosition(int floor)
+    {
+        return new Vector3(transform.position.x, floor * floorHeight, transform.position.z);
     }
 }
--- Assets/Scripts/Platform system/PlatformButton.cs
+++ Assets/Scripts/Platform system/PlatformButton.cs
@@ -1,13 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class PlatformButton : XRGrabInteractable
 {
     public Platform platform;
     public int floor;
+    private const float MOVEMENT_THRESHOLD = 0.01f;
+
+    public override bool IsSelectableBy(IXRSelectInteractor interactor)
+    {
+        if (!base.IsSelectableBy(interactor) || platform == null)
+            return false;
+
+        // Check if platform is moving
+        Vector3 currentPosition = platform.transform.position;
+        Vector3 expectedPosition = platform.floors[platform.currentFloor].position;
+        if (Vector3.Distance(currentPosition, expectedPosition) >= MOVEMENT_THRESHOLD)
+            return false;
+
+        // Check if interactor is on platform
+        var interactorPosition = interactor.transform.position;
+        return IsPositionOnPlatform(interactorPosition);
+    }
+
+    private bool IsPositionOnPlatform(Vector3 position)
+    {
+        Bounds platformBounds = platform.GetComponent<Collider>().bounds;
+        return platformBounds.Contains(position);
+    }
 
     protected override void OnSelectExiting(SelectExitEventArgs args)
     {
