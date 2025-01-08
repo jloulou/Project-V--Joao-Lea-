1249c0f674d6348c7b33ef704c26557757623828
leaaaaass <132161655+leaaaaass@users.noreply.github.com>
2025-01-08 15:57:52 +0000

    more debugg

--- Assets/Scripts/Platform system/PlatformTrigger.cs
+++ Assets/Scripts/Platform system/PlatformTrigger.cs
@@ -15,28 +15,39 @@ public class PlatformTrigger : MonoBehaviour
 
     private bool isMoving = false;
 
+    void Awake()
+    {
+        Debug.Log($"PlatformTrigger Awake on {gameObject.name}. Initial isMoving: {isMoving}");
+    }
+
+    void Start()
+    {
+        Debug.Log($"PlatformTrigger Start on {gameObject.name}. isMoving: {isMoving}, targetFloor: {targetFloor}");
+    }
+
     public void TriggerPlatform()
     {
-        Debug.Log($"TriggerPlatform called. isMoving: {isMoving}");
+        Debug.Log($"TriggerPlatform called on {gameObject.name}. isMoving: {isMoving}, targetFloor: {targetFloor}");
 
         if (!isMoving)
         {
+            Debug.Log($"Setting platform.nextFloor to {targetFloor}");
             isMoving = true;
             platform.nextFloor = targetFloor;
             onPlatformTriggered?.Invoke();
-            Debug.Log("Platform movement triggered");
+            Debug.Log($"Platform movement triggered on {gameObject.name}");
         }
         else
         {
-            Debug.Log("Platform trigger ignored because isMoving is true");
+            Debug.Log($"Platform trigger ignored on {gameObject.name} because isMoving is true");
         }
     }
 
-    // Add this method to manually reset the state
     public void ResetTriggerState()
     {
-        Debug.Log($"Resetting trigger state. Old isMoving: {isMoving}");
+        Debug.Log($"Resetting trigger state on {gameObject.name}. Old isMoving: {isMoving}");
         isMoving = false;
+        Debug.Log($"New isMoving state: {isMoving}");
     }
 
     public bool IsPlatformMoving()
@@ -46,6 +57,7 @@ public class PlatformTrigger : MonoBehaviour
 
     public void StopPlatform()
     {
+        Debug.Log($"StopPlatform called on {gameObject.name}");
         isMoving = false;
         platform.nextFloor = platform.currentFloor;
     }
