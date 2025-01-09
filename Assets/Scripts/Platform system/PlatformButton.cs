e87265335c23f53a55e4a3aadeee00380f5f3c60
leaaaaass <132161655+leaaaaass@users.noreply.github.com>
2025-01-09 00:44:55 +0000

    buttons

--- Assets/Scripts/Platform system/PlatformButton.cs
+++ Assets/Scripts/Platform system/PlatformButton.cs
@@ -1,17 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
-public class PlatformButton : XRGrabInteractable
+public class PlatformButton : XRSimpleInteractable
 {
     public Platform platform;
     public int floor;
 
-    protected override void OnSelectEntering(SelectEnterEventArgs args)
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
-        base.OnSelectEntering(args);
+        base.OnSelectEntered(args);
         platform.nextFloor = floor;
     }
 }
